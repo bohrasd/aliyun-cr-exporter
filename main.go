8b372b24ffa6fbfaa519779d7701dc940984b1fc
bohrasd <bohrasdf@gmail.com>
2021-12-02 11:57:00 +0100

    still can't scale but better

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/bohrasd/aliyun-cr-exporter/aliyun"
 	"github.com/prometheus/client_golang/prometheus"
@@ -11,71 +12,8 @@ import (
 )
 
 func main() {
-	// declare metrics
-	// namespacesGauge := prometheus.NewGaugeVec(
-	// prometheus.GaugeOpts{
-	// Name: "aliyun_acr_namespace_info",
-	// Help: "Aliyun ACR Namespace",
-	// },
-	// []string{"namespaceStatus", "namespace", "authorizeType"},
-	// )
-
-	// repoDownloadsGauge := prometheus.NewGaugeVec(
-	// prometheus.GaugeOpts{
-	// Name: "aliyun_acr_repo_downloads",
-	// Help: "Aliyun ACR Repository Downloads",
-	// },
-	// []string{ //"downloads", "gmtCreate", "gmtModified", "logo",
-	// "regionId", "repoAuthorizeType", "repoBuildType", //"repoDomainList",
-	// "repoId", "repoName", "repoNamespace", "repoStatus", "repoType", "summary"},
-	// )
-
-	// prometheus.MustRegister(namespacesGauge, repoDownloadsGauge)
 	exporter := NewAliyunExporter()
 	prometheus.MustRegister(&exporter)
-
-	// c_mutex := aliyun.NewAliyunClientMutex()
-
-	// namespaces := c_mutex.NamespacesList()
-
-	// for _, namespace := range namespaces {
-
-	//     namespacesGauge.With(prometheus.Labels{
-	//         "namespace":       namespace.Namespace,
-	//         "namespaceStatus": namespace.NamespaceStatus,
-	//         "authorizeType":   namespace.AuthorizeType,
-	//     }).Set(1)
-
-	//     ch := make(chan aliyun.Repo, 100)
-	//     go c_mutex.ReposList(namespace, ch)
-
-	//     go func(namespace aliyun.Namespace) {
-	//         for repo := range ch {
-	//             fmt.Println(repo)
-
-	//             repoDownloadsGauge.With(prometheus.Labels{
-	//                 // "downloads": repo.Downloads,
-	//                 // "gmtCreate": repo.GmtCreate,
-	//                 // "gmtModified": repo.GmtModified,
-	//                 // "logo": repo.Logo,
-	//                 "regionId":          repo.RegionId,
-	//                 "repoAuthorizeType": repo.RepoAuthorizeType,
-	//                 "repoBuildType":     repo.RepoBuildType,
-	//                 // "repoDomainList": repo.RepoDomainList,
-	//                 "repoId":        strconv.FormatInt(repo.RepoId, 10),
-	//                 "repoName":      repo.RepoName,
-	//                 "repoNamespace": repo.RepoNamespace,
-	//                 // "repoOriginType": repo.RepoOriginType,
-	//                 "repoStatus": repo.RepoStatus,
-	//                 "repoType":   repo.RepoType,
-	//                 // "stars": repo.Stars,
-	//                 "summary": repo.Summary,
-	//             }).Set(float64(repo.Downloads))
-
-	//         }
-	//     }(namespace)
-
-	// }
 
 	// serve metrics
 	http.Handle("/metrics", promhttp.Handler())
@@ -106,16 +44,27 @@ type MetricMap map[string]*prometheus.Desc
 
 type AliyunExporter struct {
 	instance   string
-	ac         *aliyun.AliyunClientMutex
 	metric_map MetricMap
 	// mu         sync.Mutex
+}
+
+type Datas struct {
+	namespaces map[string]aliyun.Namespace
+	repos      map[int64]aliyun.Repo
+	repoBuilds map[int64][]aliyun.Build
+	repoTotal  map[int64]int
+}
+
+var data = Datas{
+	namespaces: make(map[string]aliyun.Namespace),
+	repos:      make(map[int64]aliyun.Repo),
+	repoBuilds: make(map[int64][]aliyun.Build),
+	repoTotal:  make(map[int64]int),
 }
 
 func NewAliyunExporter() AliyunExporter {
 
 	ae := AliyunExporter{
-
-		ac: aliyun.NewAliyunClientMutex(),
 
 		metric_map: MetricMap{
 			"aliyun_acr_namespace_info": prometheus.NewDesc("aliyun_acr_namespace_info", "Aliyun ACR Namespace",
@@ -125,6 +74,37 @@ func NewAliyunExporter() AliyunExporter {
 
 	ae.metric_map.initRepoDisc()
 	ae.metric_map.initBuildDisc()
+
+	go func() {
+		ac := aliyun.NewAliyunClientMutex()
+		namespaces := ac.NamespacesList()
+		limit := make(chan struct{}, 10)
+
+		multi_chan := map[string]chan aliyun.Repo{}
+		for _, namespace := range namespaces {
+
+			data.namespaces[namespace.Namespace] = namespace
+			multi_chan[namespace.Namespace] = make(chan aliyun.Repo, 100)
+
+			go func() {
+				ac.ReposList(namespace, multi_chan[namespace.Namespace])
+			}()
+
+			for repo := range multi_chan[namespace.Namespace] {
+				data.repos[repo.RepoId] = repo
+				if repo.RepoBuildType == "AUTO_BUILD" {
+
+					limit <- struct{}{}
+					data.repoBuilds[repo.RepoId], data.repoTotal[repo.RepoId] = ac.GetLastestBuilds(namespace.Namespace, repo.RepoName)
+
+					<-limit
+				}
+
+			}
+		}
+
+		<-time.Tick(time.Second * 30)
+	}()
 
 	return ae
 
@@ -143,30 +123,21 @@ func (exporter *AliyunExporter) Collect(prom_ch chan<- prometheus.Metric) {
 	// exporter.mu.Lock()
 	// defer exporter.mu.Unlock()
 
-	namespaces := exporter.ac.NamespacesList()
+	namespaces := data.namespaces
 
-	multi_chan := map[string]chan aliyun.Repo{}
 	for _, namespace := range namespaces {
 
 		prom_ch <- prometheus.MustNewConstMetric(exporter.metric_map["aliyun_acr_namespace_info"], prometheus.GaugeValue, 1,
 			namespace.Namespace, namespace.NamespaceStatus, namespace.AuthorizeType)
 
-		multi_chan[namespace.Namespace] = make(chan aliyun.Repo, 100)
+	}
+	for _, repo := range data.repos {
+		if repo.RepoBuildType == "AUTO_BUILD" {
+			exporter.metric_map.collectBuildTotal(prom_ch, repo, data.repoTotal[repo.RepoId])
+			exporter.metric_map.collectBuildMetric(prom_ch, data.repoBuilds[repo.RepoId])
 
-		go func(namespace aliyun.Namespace) {
-			exporter.ac.ReposList(namespace, multi_chan[namespace.Namespace])
-		}(namespace)
-
-		for repo := range multi_chan[namespace.Namespace] {
-			if repo.RepoBuildType == "AUTO_BUILD" {
-
-				build, total := exporter.ac.GetLastestBuilds(namespace.Namespace, repo.RepoName)
-				exporter.metric_map.collectBuildTotal(prom_ch, repo, total)
-				exporter.metric_map.collectBuildMetric(prom_ch, build)
-
-			}
-
-			exporter.metric_map.collectRepoMetric(prom_ch, repo)
 		}
+
+		exporter.metric_map.collectRepoMetric(prom_ch, repo)
 	}
 }
